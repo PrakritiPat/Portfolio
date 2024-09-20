ee7752ad0f13287b205cb7e0ce2cda72ef8a4187
PrakritiPat <134564716+PrakritiPat@users.noreply.github.com>
2024-09-20 11:38:00 -0700

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ExperienceComponent } from './experience/experience.component';
+import { HomeComponent } from './home/home.component';
+import { ProjectComponent } from './project/project.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ExperienceComponent,
+    HomeComponent,
+    ProjectComponent
   ],
   imports: [
     BrowserModule,

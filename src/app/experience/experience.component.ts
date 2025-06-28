11aaa64d84b5fbcd0f5b60bf723f9375f662f2d1
Prakriti Patnaik <prakritipatnaik123@gmail.com>
2025-06-27 20:46:23 -0700

    new updates 06/27/25

--- src/app/experience/experience.component.ts
+++ src/app/experience/experience.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./experience.component.css']
 })
 export class ExperienceComponent {
+  scrollTo(element: HTMLElement) {
+    const yOffset = 200; // adjust this based on your navbar height (e.g. -80 or -90 for margin)
 
+    element.scrollIntoView({ behavior: 'smooth', block: 'start' });
+  }
 }
